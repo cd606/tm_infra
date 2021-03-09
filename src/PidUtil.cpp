a7292cc96561b11206ead649e4db404f75a4899e
cd606 <cd606dc@gmail.com>
2021-03-09 17:24:27 -0600

    add a utility function

--- src/PidUtil.cpp
+++ src/PidUtil.cpp
@@ -5,7 +5,10 @@
 #include <processthreadsapi.h>
 #else
 #include <sys/types.h>
+#include <sys/stat.h>
 #include <unistd.h>
+#include <iostream>
+#include <sstream>
 #endif
 
 namespace dev { namespace cd606 { namespace tm { namespace infra { namespace pid_util {
@@ -14,6 +17,18 @@ namespace dev { namespace cd606 { namespace tm { namespace infra { namespace pid
             return (int64_t) GetCurrentProcessId();
         #else
             return (int64_t) ::getpid();
+        #endif
+    }
+    bool pidIsRunning(int64_t pid) {
+        #ifdef _MSC_VER
+            DWORD status;
+            GetExitCodeProcess((HANDLE) pid, &status);
+            return (status == STILL_ACTIVE);
+        #else
+            std::ostringstream oss;
+            oss << "/proc/" << pid;
+            struct stat buffer;
+            return (stat(oss.str().c_str(), &buffer) == 0);
         #endif
     }
 } } } } }
--- tm_kit/infra/PidUtil.hpp
+++ tm_kit/infra/PidUtil.hpp
@@ -5,6 +5,7 @@
 
 namespace dev { namespace cd606 { namespace tm { namespace infra { namespace pid_util {
     extern int64_t getpid();
+    extern bool pidIsRunning(int64_t pid);
 } } } } }
 
 #endif
