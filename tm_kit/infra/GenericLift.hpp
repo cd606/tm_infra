245ae6fe8f0550067a172adb4edd460a2d5d2fba
cd606 <cd606dc@gmail.com>
2021-06-19 11:54:51 -0500

    msvc build fix

--- tm_kit/infra/GenericLift.hpp
+++ tm_kit/infra/GenericLift.hpp
@@ -52,31 +52,56 @@ namespace dev { namespace cd606 { namespace tm { namespace infra {
         public:
             using InputType = typename Types<T>::InputType;
             using OutputType = typename Types<T>::OutputType;
+#ifdef _MSC_VER
+            static constexpr bool IsBareFunction = false;
+#endif
         };
         template <class A, class B>
         class GenericLiftTypeFinder<A (*)(B)> {
         public:
             using InputType = std::decay_t<B>;
             using OutputType = std::decay_t<A>;
+#ifdef _MSC_VER
+            static constexpr bool IsBareFunction = false;
+#endif
         };
         template <class A, class B>
         class GenericLiftTypeFinder<A (&)(B)> {
         public:
             using InputType = std::decay_t<B>;
             using OutputType = std::decay_t<A>;
+#ifdef _MSC_VER
+            static constexpr bool IsBareFunction = false;
+#endif
+        };
+        template <class A, class B>
+        class GenericLiftTypeFinder<A(B)> {
+        public:
+            using InputType = std::decay_t<B>;
+            using OutputType = std::decay_t<A>;
+#ifdef _MSC_VER
+            static constexpr bool IsBareFunction = true;
+#endif
         };
         template <class A, class B>
         class GenericLiftTypeFinder<std::function<A(B)>> {
         public:
             using InputType = std::decay_t<B>;
             using OutputType = std::decay_t<A>;
+#ifdef _MSC_VER
+            static constexpr bool IsBareFunction = false;
+#endif
         };
         template <class X>
         struct IsInnerData {
             static constexpr bool Value = false;
         };
         template <class T>
+    #if _MSC_VER
+        struct IsInnerData<TimedDataWithEnvironment<T, typename M::StateType, typename M::TimePoint>> {
+    #else
         struct IsInnerData<typename M::template InnerData<T>> {
+    #endif
             static constexpr bool Value = true;
         };
         
@@ -108,7 +133,11 @@ namespace dev { namespace cd606 { namespace tm { namespace infra {
             }
         };
         template <class A, class B>
+    #if _MSC_VER
+        class GenericLiftImpl<TimedDataWithEnvironment<A, typename M::StateType, typename M::TimePoint>, std::optional<TimedDataWithEnvironment<B, typename M::StateType, typename M::TimePoint>>, true> {
+    #else
         class GenericLiftImpl<typename M::template InnerData<A>, typename M::template Data<B>, true> {
+    #endif
         public:
             template <class F>
             static auto lift(F &&f, LiftParameters<typename M::TimePoint> const &liftParam) {
@@ -124,15 +153,27 @@ namespace dev { namespace cd606 { namespace tm { namespace infra {
             }
         };
         template <class A>
+    #ifdef _MSC_VER
+        class GenericLiftImpl<A, void, true> {
+    #else
         class GenericLiftImpl<typename M::template InnerData<A>, void, true> {
+    #endif
         public:
             template <class F>
             static auto lift(F &&f, LiftParameters<typename M::TimePoint> const &liftParam) {
-                return M::template simpleExporter<A>(std::move(f), liftParam);
+    #ifdef _MSC_VER
+                return M::template simpleExporter<typename A::ValueType>(std::move(f), liftParam);
+    #else
+                return M::template simpleExporter<A>(std::move(f), liftParam); 
+    #endif
             }
         };
         template <class A>
+    #ifdef _MSC_VER 
+        class GenericLiftImpl<typename M::EnvironmentType *, std::tuple<bool, std::optional<TimedDataWithEnvironment<A, typename M::StateType, typename M::TimePoint>>>, false> {
+    #else
         class GenericLiftImpl<typename M::EnvironmentType *, std::tuple<bool, typename M::template Data<A>>, false> {
+    #endif
         public:
             template <class F>
             static auto lift(F &&f, LiftParameters<typename M::TimePoint> const &liftParam) {
@@ -159,7 +200,11 @@ namespace dev { namespace cd606 { namespace tm { namespace infra {
             }
         };
         template <class A, class B>
+    #ifdef _MSC_VER
+        class GenericLiftMultiImpl<TimedDataWithEnvironment<A, typename M::StateType, typename M::TimePoint>, std::optional<TimedDataWithEnvironment<std::vector<B>, typename M::StateType, typename M::TimePoint>>> {
+    #else
         class GenericLiftMultiImpl<typename M::template InnerData<A>, typename M::template Data<std::vector<B>>> {
+    #endif
         public:
             template <class F>
             static auto lift(F &&f, LiftParameters<typename M::TimePoint> const &liftParam) {
@@ -169,17 +214,33 @@ namespace dev { namespace cd606 { namespace tm { namespace infra {
     public:
         template <class F>
         static auto lift(F &&f, LiftParameters<typename M::TimePoint> const &liftParam = LiftParameters<typename M::TimePoint> {}) {
-            return GenericLiftImpl<
-                typename GenericLiftTypeFinder<F>::InputType
-                , typename GenericLiftTypeFinder<F>::OutputType
-            >::template lift<F>(std::move(f), liftParam);
+#ifdef _MSC_VER            
+            if constexpr (GenericLiftTypeFinder<F>::IsBareFunction) {
+                return lift<F*>(&f, liftParam);
+            } else {
+#endif
+                return GenericLiftImpl<
+                    typename GenericLiftTypeFinder<F>::InputType
+                    , typename GenericLiftTypeFinder<F>::OutputType
+                >::template lift<F>(std::move(f), liftParam);
+#ifdef _MSC_VER
+            }
+#endif
         }
         template <class F>
         static auto liftMulti(F &&f, LiftParameters<typename M::TimePoint> const &liftParam = LiftParameters<typename M::TimePoint> {}) {
-            return GenericLiftMultiImpl<
-                typename GenericLiftTypeFinder<F>::InputType
-                , typename GenericLiftTypeFinder<F>::OutputType
-            >::template lift<F>(std::move(f), liftParam);
+#ifdef _MSC_VER            
+            if constexpr (GenericLiftTypeFinder<F>::IsBareFunction) {
+                return liftMulti<F*>(&f, liftParam);
+            } else {
+#endif
+                return GenericLiftMultiImpl<
+                    typename GenericLiftTypeFinder<F>::InputType
+                    , typename GenericLiftTypeFinder<F>::OutputType
+                >::template lift<F>(std::move(f), liftParam);
+#ifdef _MSC_VER
+            }
+#endif
         }
         template <class F>
         static auto lift(LiftAsMulti &&, F &&f, LiftParameters<typename M::TimePoint> const &liftParam = LiftParameters<typename M::TimePoint> {}) {
