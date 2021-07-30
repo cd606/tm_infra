059c72a66b6919219d35e69160d08390e347b862
cd606 <cd606dc@gmail.com>
2021-07-30 11:48:02 -0500

    add app classifier

---
+++ tm_kit/infra/AppClassifier.hpp
@@ -0,0 +1,50 @@
+#ifndef TM_KIT_INFRA_APP_CLASSIFIER_HPP_
+#define TM_KIT_INFRA_APP_CLASSIFIER_HPP_
+
+#include <tm_kit/infra/BasicWithTimeApp.hpp>
+#include <tm_kit/infra/RealTimeApp.hpp>
+#include <tm_kit/infra/SinglePassIterationApp.hpp>
+#include <tm_kit/infra/TopDownSinglePassIterationApp.hpp>
+
+namespace dev { namespace cd606 { namespace tm { namespace infra {
+    enum class AppClassification {
+        Empty 
+        , BasicWithTime = Empty
+        , RealTime
+        , SinglePass 
+        , SinglePassIteration = SinglePass 
+        , TopDown
+        , TopDownSinglePass = TopDown 
+        , TopDownSinglePassIteration = TopDown
+    };
+
+    template <class M>
+    class AppClassifier {};
+
+    template <class Env>
+    class AppClassifier<BasicWithTimeApp<Env>> {
+    public:
+        static constexpr AppClassification TheClassification = AppClassification::Empty;
+    };
+    template <class Env>
+    class AppClassifier<RealTimeApp<Env>> {
+    public:
+        static constexpr AppClassification TheClassification = AppClassification::RealTime;
+    };
+    template <class Env>
+    class AppClassifier<SinglePassIterationApp<Env>> {
+    public:
+        static constexpr AppClassification TheClassification = AppClassification::SinglePass;
+    };
+    template <class Env>
+    class AppClassifier<TopDownSinglePassIterationApp<Env>> {
+    public:
+        static constexpr AppClassification TheClassification = AppClassification::TopDown;
+    };
+
+    template <class M>
+    constexpr AppClassification app_classification_v = AppClassifier<M>::TheClassification;
+
+} } } }
+
+#endif
--- tm_kit/infra/SynchronousRunner.hpp
+++ tm_kit/infra/SynchronousRunner.hpp
@@ -6,6 +6,7 @@
 #include <tm_kit/infra/RealTimeApp.hpp>
 #include <tm_kit/infra/SinglePassIterationApp.hpp>
 #include <tm_kit/infra/TopDownSinglePassIterationApp.hpp>
+#include <tm_kit/infra/AppClassifier.hpp>
 
 #include <unordered_set>
 
@@ -188,12 +189,18 @@ namespace dev { namespace cd606 { namespace tm { namespace infra {
         SynchronousFacilityStreamer<T1,T2> facilityStreamer(
             std::shared_ptr<typename M::template OnOrderFacility<T1,T2>> const &facility
         ) {
+            static_assert(
+                app_classification_v<M> != AppClassification::SinglePass
+                , "single pass iteration app does not support synchronous facility streamer" 
+            );
+            /*
             if constexpr(!std::is_same_v<M, infra::RealTimeApp<typename M::EnvironmentType>>) {
                 static_assert(
                     !std::is_same_v<M, infra::SinglePassIterationApp<typename M::EnvironmentType>>
                     , "single pass iteration app does not support synchronous facility streamer" 
                 );
             }
+            */
             {
                 std::lock_guard<std::mutex> _(mutex_);
                 components_.insert(std::static_pointer_cast<void>(facility));
